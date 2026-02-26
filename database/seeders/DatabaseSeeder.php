004336d999d9f67fba3ddb58d0d39c930af2d960
Filemon Mayordomo <filemonmayordomo680@gmail.com>
2026-02-26 20:35:42 +0800

    Fix seeder selling_price

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,16 +42,16 @@ class DatabaseSeeder extends Seeder
 
         // Create sample products
         $products = [
-            ['name' => 'Desktop PC',         'category' => 'Computers',   'price' => 25000, 'stock' => 10],
-            ['name' => 'Laptop',             'category' => 'Computers',   'price' => 35000, 'stock' => 8],
-            ['name' => 'Mechanical Keyboard','category' => 'Peripherals', 'price' => 2500,  'stock' => 20],
-            ['name' => 'Gaming Mouse',       'category' => 'Peripherals', 'price' => 1500,  'stock' => 25],
-            ['name' => 'Monitor 24"',        'category' => 'Peripherals', 'price' => 8000,  'stock' => 12],
-            ['name' => 'USB Hub',            'category' => 'Accessories', 'price' => 800,   'stock' => 30],
-            ['name' => 'HDMI Cable',         'category' => 'Accessories', 'price' => 350,   'stock' => 50],
-            ['name' => 'Antivirus 1yr',      'category' => 'Software',    'price' => 1200,  'stock' => 15],
-            ['name' => 'WiFi Router',        'category' => 'Networking',  'price' => 3500,  'stock' => 10],
-            ['name' => 'Network Switch',     'category' => 'Networking',  'price' => 2800,  'stock' => 7],
+            ['name' => 'Desktop PC',          'category' => 'Computers',   'selling_price' => 25000, 'stock' => 10],
+            ['name' => 'Laptop',              'category' => 'Computers',   'selling_price' => 35000, 'stock' => 8],
+            ['name' => 'Mechanical Keyboard', 'category' => 'Peripherals', 'selling_price' => 2500,  'stock' => 20],
+            ['name' => 'Gaming Mouse',        'category' => 'Peripherals', 'selling_price' => 1500,  'stock' => 25],
+            ['name' => 'Monitor 24"',         'category' => 'Peripherals', 'selling_price' => 8000,  'stock' => 12],
+            ['name' => 'USB Hub',             'category' => 'Accessories', 'selling_price' => 800,   'stock' => 30],
+            ['name' => 'HDMI Cable',          'category' => 'Accessories', 'selling_price' => 350,   'stock' => 50],
+            ['name' => 'Antivirus 1yr',       'category' => 'Software',    'selling_price' => 1200,  'stock' => 15],
+            ['name' => 'WiFi Router',         'category' => 'Networking',  'selling_price' => 3500,  'stock' => 10],
+            ['name' => 'Network Switch',      'category' => 'Networking',  'selling_price' => 2800,  'stock' => 7],
         ];
 
         foreach ($products as $p) {
@@ -61,8 +61,10 @@ class DatabaseSeeder extends Seeder
                     ['name' => $p['name']],
                     [
                         'category_id'    => $category->id,
-                        'price'          => $p['price'],
+                        'selling_price'  => $p['selling_price'],
+                        'cost_price'     => 0,
                         'stock_quantity' => $p['stock'],
+                        'sku'            => strtoupper(substr(str_replace(' ', '', $p['name']), 0, 8)) . rand(100,999),
                         'is_active'      => true,
                     ]
                 );
